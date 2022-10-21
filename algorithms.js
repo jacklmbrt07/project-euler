279cec701c11fe70773f307d9580a88b4d6ffead
Jack Lambert <jacklmbrt07@gmail.com>
2022-10-21 11:01:19 -0700

    added algo 4

--- algorithms.js
+++ algorithms.js
@@ -120,3 +120,51 @@ function permAlone(str) {
 }
 
 permAlone("aab");
+
+/**4. Pairwise
+Given an array arr, find element pairs whose sum equal the second argument arg and return the sum of their indices.
+
+You may use multiple pairs that have the same numeric elements but different indices. Each pair should use the lowest possible available indices. Once an element has been used it cannot be reused to pair with another element. For instance, pairwise([1, 1, 2], 3) creates a pair [2, 1] using the 1 at index 0 rather than the 1 at index 1, because 0+2 < 1+2.
+
+For example pairwise([7, 9, 11, 13, 15], 20) returns 6. The pairs that sum to 20 are [7, 13] and [9, 11]. We can then write out the array with their indices and values.
+
+Index	0	1	2	3	4
+Value	7	9	11	13	15
+Below we'll take their corresponding indices and add them.
+
+7 + 13 = 20 → Indices 0 + 3 = 3
+9 + 11 = 20 → Indices 1 + 2 = 3
+3 + 3 = 6 → Return 6 */
+
+function pairwise(arr, arg) {
+  let arrCopy = arr;
+  let indicesArr = [];
+  let index = 0;
+
+  while (arrCopy.length != 0) {
+    for (let i = 1; i < arrCopy.length; i++) {
+      if (
+        arrCopy[0] + arrCopy[i] === arg &&
+        !indicesArr.includes(index) &&
+        !indicesArr.includes(index + i)
+      ) {
+        indicesArr.push(index);
+        indicesArr.push(index + i);
+      }
+    }
+
+    arrCopy.shift();
+
+    index++;
+  }
+
+  function add(acc, a) {
+    return acc + a;
+  }
+
+  const sum = indicesArr.reduce(add, 0);
+
+  return sum;
+}
+
+pairwise([0, 0, 0, 0, 1, 1], 1);
